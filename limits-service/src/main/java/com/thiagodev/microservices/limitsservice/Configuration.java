cdb5d0cd80ea2b2c3278210c7eaf7101717f794c
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-10-12 17:55:03 -0300

    changing config values

--- limits-service/src/main/java/com/thiagodev/microservices/limitsservice/Configuration.java
+++ limits-service/src/main/java/com/thiagodev/microservices/limitsservice/Configuration.java
@@ -16,5 +16,11 @@ public class Configuration {
 	public int getMinimum() {
 		return minimum;
 	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
 
 }
