5f461aa6841473a8c44484b33a5ab5d4ba60449f
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-10-14 12:35:40 -0300

    using feign

--- currency-conversion-service/src/main/java/com/thiagodev/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/thiagodev/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,7 +2,9 @@ package com.thiagodev.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.thiagodev.microservices.currencyconversionservice")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
