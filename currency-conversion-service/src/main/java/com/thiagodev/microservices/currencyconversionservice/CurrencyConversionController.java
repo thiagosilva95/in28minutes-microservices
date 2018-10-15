88e109ddab338de0f427109a0affcd5b66565fd2
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-10-15 20:59:50 -0300

    adding zipkin tracer

--- currency-conversion-service/src/main/java/com/thiagodev/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/thiagodev/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
@@ -43,6 +47,8 @@ public class CurrencyConversionController {
 												  @PathVariable BigDecimal quantity) {
 		
 		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+		
+		logger.info("{}", response);
 		
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), 
 				quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
--- currency-exchange-service/src/main/java/com/thiagodev/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/thiagodev/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.thiagodev.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +19,18 @@ public class CurrencyExchangeController {
 	@Autowired
 	private ExchangeValueRepository repository;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		ExchangeValue exchangeValue = 
+				repository.findByFromAndTo(from, to);
+		
 		exchangeValue.setPort(
 				Integer.parseInt(environment.getProperty("local.server.port")));
+		
+		logger.info("{}", exchangeValue);
+		
 		return exchangeValue;
 	}
 }
